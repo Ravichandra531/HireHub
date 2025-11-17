a171cc57bfe1d7cd9b94b41aafce9223bc675d88
Ravichandra531 <ruchitrshinde@gmail.com>
2025-11-17 17:25:22 +0530

    h

--- Desktop/HireHub/frontend/src/pages/api.js
+++ Desktop/HireHub/frontend/src/pages/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = "https://hirehub-1-ddku.onrender.com/api"
 
 export async function apiPost(endpoint, body) {
   const res = await fetch(`${API_URL}${endpoint}`, {
