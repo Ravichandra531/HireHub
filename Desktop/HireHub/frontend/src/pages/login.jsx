4bb308566488ab46a6d5c5771c7c6172523ed58a
Ravichandra531 <ruchitrshinde@gmail.com>
2025-11-17 17:42:15 +0530

    h

--- Desktop/HireHub/frontend/src/pages/login.jsx
+++ Desktop/HireHub/frontend/src/pages/login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
     e.preventDefault();
     setMsg("");
 
-    const { ok, data } = await apiPost("/auth/login", form);
+    const { ok, data } = await apiPost("/api/auth/login", form);
 
     if (!ok) {
       setMsg(data.error);
